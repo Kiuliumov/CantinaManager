60ead87402e2b233d5d6296b1f0ab6fae16a2a71
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-15 11:31:47 +0200

    Create AppDbContext.cs

---
+++ api/CantinaManager/CantinaManager/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CantinaManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CantinaManager.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Task> Tasks => Set<Task>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Task>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+
+}

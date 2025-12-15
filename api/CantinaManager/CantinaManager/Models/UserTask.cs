a7cebc47de543c65afb3393b7e94824561970571
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-15 14:23:12 +0200

    Create UserTask.cs

---
+++ api/CantinaManager/CantinaManager/Models/UserTask.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CantinaManager.Models
+{
+    public class UserTask
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, MaxLength(500)]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public string UserId { get; set; } 
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}

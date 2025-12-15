a5973f0173ffb2fb6865867e5040b4ecbd0419b4
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-15 11:31:41 +0200

    Create User.cs

---
+++ api/CantinaManager/CantinaManager/Auth/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CantinaManager.Models
+{
+    public class User : IdentityUser
+    {
+        // Extra profile fields
+        public string FullName { get; set; } = string.Empty;
+        public string? ProfilePictureUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation property: tasks assigned to the user
+        public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+    }
+}

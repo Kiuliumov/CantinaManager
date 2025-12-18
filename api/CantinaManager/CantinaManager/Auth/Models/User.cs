6e7e536d9e1868d9ab183403e8c36eebbb52eeb2
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-18 14:48:22 +0200

    Update User.cs

--- api/CantinaManager/CantinaManager/Auth/Models/User.cs
+++ api/CantinaManager/CantinaManager/Auth/Models/User.cs
@@ -6,9 +6,13 @@ namespace CantinaManager.Models
     public class User : IdentityUser
     {
         public string FullName { get; set; } = string.Empty;
+        
         public string? ProfilePictureUrl { get; set; }
+        
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
         public virtual ICollection<UserTask> UserTasks { get; set; } = new List<UserTask>();
+        
         public ICollection<IdentityUserRole<string>> UserRoles { get; set; } = new List<IdentityUserRole<string>>();
 
     }

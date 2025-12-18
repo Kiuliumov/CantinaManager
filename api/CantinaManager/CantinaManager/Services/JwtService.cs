0ffa34fdb2838fc3fb4712b6aa6299c6ad6b2479
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-18 13:52:48 +0200

    Create JwtService.cs

---
+++ api/CantinaManager/CantinaManager/Services/JwtService.cs
@@ -0,0 +1,48 @@
+﻿using CantinaManager.Models;
+
+
+namespace CantinaManager.Services
+{
+    public class JwtService : ITokenService    
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
+
+        public JwtService(IConfiguration config, UserManager<User> userManager)
+        {
+            _config = config;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateAccessTokenAsync(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(_config["Jwt:ExpireMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+        }
+    }
+}

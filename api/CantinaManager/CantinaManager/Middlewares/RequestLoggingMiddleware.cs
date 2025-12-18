cad1b012a1bd8784f18272625e4a08ad8a9f541c
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-18 14:24:54 +0200

    Update RequestLoggingMiddleware.cs

--- api/CantinaManager/CantinaManager/Middlewares/RequestLoggingMiddleware.cs
+++ api/CantinaManager/CantinaManager/Middlewares/RequestLoggingMiddleware.cs
@@ -1,6 +1,24 @@
-﻿namespace CantinaManager.Middlewares
+﻿public class RequestLoggingMiddleware
 {
-    public class RequestLogging
+    private readonly RequestDelegate _next;
+
+    public RequestLoggingMiddleware(RequestDelegate next)
     {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine($"Incoming Request: {context.Request.Method} {context.Request.Path}");
+        await _next(context); 
+        Console.WriteLine($"Response Status: {context.Response.StatusCode}");
+    }
+}
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestLoggingMiddleware>();
     }
 }

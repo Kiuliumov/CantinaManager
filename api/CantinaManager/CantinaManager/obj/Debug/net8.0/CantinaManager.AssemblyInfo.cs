561e40162e59dc460458b891447b942973dcd248
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-19 17:20:24 +0200

    Update CantinaManager.AssemblyInfo.cs

--- api/CantinaManager/CantinaManager/obj/Debug/net8.0/CantinaManager.AssemblyInfo.cs
+++ api/CantinaManager/CantinaManager/obj/Debug/net8.0/CantinaManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7b3526c05fb4164464fdd4608649b9a044820b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a401dbccefa80ea9a88dba5629ce8ec5182a0fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

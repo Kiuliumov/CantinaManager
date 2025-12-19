fa0ad222de758a85bd3dc65655b0954fb9fb70a1
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-19 11:56:21 +0200

    Create UserRole.cs

---
+++ api/CantinaManager/CantinaManager/Auth/Models/UserRole.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CantinaManager.Models
+{
+    public class UserRole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string RoleName { get; set; } = string.Empty;
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; } = null!;
+    }
+}
